3c7992817c165bd08ff400b47ab67149cc78840a
sadikhossain69 <sadikhossain433@gmail.com>
2022-04-03 02:41:19 +0600

    successfully get api

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Coins = () => {
+
+    const [coins, setCoins] = useState([])
+
+    useEffect( () => {
+        axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
+        .then( (res) => {
+            setCoins(res.data);
+        } )
+    } , [])
+
     return (
         <div>
-            <h2>This is Coins</h2>
+            <h2>This is Coins {coins.length} </h2>
         </div>
     );
 };
